ca5f76327c0b58edabd1b8667d8695205f075a51
SelfDirectedLearningRobb <robertdeforest@gmail.com>
2017-06-12 21:31:06 -0700

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,9 @@ from . import views
 #if someone requests the blog directory serve them the post_list view page
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
+	#if someone requests a specific post (as defined in the post_detail function in views.py) serve them the post_detail view page
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+	#add a link to the Post_Form when the link is clicked to create a new post
+	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 #import the timezone functionality in order to be able to sort by published date, etc.
 from django.utils import timezone
 #import the Post class from the models.py file in the same directory 
 #(since it is in the same directory we can omit the .py file extension)
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -15,5 +16,33 @@ def post_list(request):
 	return render(request, 'blog/post_list.html', {'posts':posts})
 	
 def post_detail(request, pk):
+	#assign an instance 'post' of the 'Post' class as defined in models.py where the post key or pk specifies the specific post
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post':post})
+	
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm()
+		return render(request, 'blog/post_edit.html',{'form': form})
+		
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
